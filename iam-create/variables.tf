60537b9d2db83492fbeefe6a3e61256f5b92e922
Fernando Jimenez <wizardinaction@gmail.com>
2020-12-22 13:01:55 -0700

    Remove tag along variables

--- iam-create/variables.tf
+++ iam-create/variables.tf
@@ -15,33 +15,3 @@ variable "region2" {
 variable "tfstatebucket" {
   type = string
 }
-
-variable "subnet_ids" {
-  description = "Existing subnets"
-  type        = list(any)
-}
-
-variable "first-contact" {
-  description = "Deploy zero day access ssh public key"
-  type        = map(any)
-}
-
-variable "instance-sg" {
-  description = "Security Groups for the app"
-  type        = list(string)
-}
-
-variable "instance-type" {
-  description = "AWS instance type"
-  type        = string
-}
-
-variable "ami-id" {
-  description = "AWS AMI"
-  type        = string
-}
-
-variable "instances" {
-  description = "Count of instances wanted"
-  type        = number
-}
