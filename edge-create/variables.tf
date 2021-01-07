7e574e4049e5f3844ffb227cb6aaa80f5c462488
Fernando Jimenez <na@na.na>
2021-01-07 16:14:11 -0700

    Create tf for edge

---
+++ edge-create/variables.tf
@@ -0,0 +1,47 @@
+variable "profile" {
+  type = string
+}
+
+variable "region1" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "region2" {
+  type    = string
+  default = "us-west-2"
+}
+
+variable "tfstatebucket" {
+  type = string
+}
+
+variable "subnet_ids" {
+  description = "Existing subnets"
+  type        = list(any)
+}
+
+variable "first-contact" {
+  description = "Deploy zero day access ssh public key"
+  type        = map(any)
+}
+
+variable "instance-sg" {
+  description = "Security Groups for the app"
+  type        = list(string)
+}
+
+variable "instance-type" {
+  description = "AWS instance type"
+  type        = string
+}
+
+variable "vpc-id" {
+  description = "VPC id"
+  type        = string
+}
+
+variable "external_ip" {
+  description = "IP range to allow ssh"
+  type        = string
+}
