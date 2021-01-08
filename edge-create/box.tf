3e183f44532946e2c8de08feb33a8a9f4739923b
Fernando Jimenez <na@na.na>
2021-01-07 17:00:34 -0700

    Re-install the edge-of-tomorrow security group and remove the testing one

--- edge-create/box.tf
+++ edge-create/box.tf
@@ -8,8 +8,7 @@ resource "aws_instance" "box" {
   ami           = data.aws_ami.ubuntu-20-04.id
   instance_type = var.instance-type
   subnet_id     = element(var.subnet_ids, 0)
-  #  vpc_security_group_ids      = [ aws_security_group.edge-of-tomorrow.id ]
-  vpc_security_group_ids      = ["sg-099d4e6e"]
+  vpc_security_group_ids      = [ aws_security_group.edge-of-tomorrow.id ]
   key_name                    = aws_key_pair.first-contact.key_name
   associate_public_ip_address = true
 
