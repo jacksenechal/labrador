ce391bd83183aedb9cfc5e828e9498b23f5c7827
Fernando Jimenez <fjimenez@mavenlink.com>
2021-01-20 10:13:05 -0700

    Match security group resource name

--- controller-create/box.tf
+++ controller-create/box.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "box" {
   ami                         = data.aws_ami.ubuntu-20-04.id
   instance_type               = var.instance-type
   subnet_id                   = element(var.subnet_ids, 0)
-  vpc_security_group_ids      = [aws_security_group.tf_controller.id]
+  vpc_security_group_ids      = [aws_security_group.tfcontroller.id]
   key_name                    = aws_key_pair.first-contact.key_name
   associate_public_ip_address = true
   iam_instance_profile = aws_iam_instance_profile.controller_profile.name 
