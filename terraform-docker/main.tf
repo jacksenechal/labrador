2cdf4bcf6941cfe8ec97482984a6da64f0f6e316
Fernando Jimenez <20340262+ludusregio@users.noreply.github.com>
2021-02-16 09:58:11 -0700

    Use random_string

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -13,21 +13,51 @@ resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
+resource "random_string" "random" {
+  length = 5
+  special = false
+  upper = false
+}
+resource "random_string" "random2" {
+  length = 5
+  special = false
+  upper = false
+}
+
 resource "docker_container" "node_container" {
-  name  = "nodered"
+  name  = join("-", ["nodered", random_string.random.result])
   image = docker_image.nodered_image.latest
   ports {
     internal = 1880
-    external = 1880
+#    external = 1880
+  }
+}
+
+resource "docker_container" "node_container2" {
+  name  = join("-", ["nodered", random_string.random2.result])
+  image = docker_image.nodered_image.latest
+  ports {
+    internal = 1880
+#    external = 1880
   }
 }
 
 output "IP-Address" {
-  value       = join(":", [docker_container.node_container.ip_address, docker_container.node_container.ports[0].external])
+  value       =  join(":", [docker_container.node_container.ip_address, docker_container.node_container.ports[0].external])
   description = "The IP address and external port of the container"
 }
 
 output "container-name" {
   value       = docker_container.node_container.name
   description = "The name of the container"
+}
+
+output "IP-Address2" {
+  value       =  join(":", [docker_container.node_container2.ip_address, docker_container.node_container2.ports[0].external])
+  description = "The IP address and external port of the container"
+}
+
+output "container-name2" {
+  value       = docker_container.node_container2.name
+  description = "The name of the container2"
 }
