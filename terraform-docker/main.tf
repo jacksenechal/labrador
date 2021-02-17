f2626d3319ace63669ef26cf5a1d4ec5991b3df6
Fernando Jimenez <20340262+ludusregio@users.noreply.github.com>
2021-02-16 18:35:40 -0700

    Start using variables

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -9,32 +9,49 @@ terraform {
 
 provider "docker" {}
 
+variable "external_port" {
+    type = number
+    default = 1880
+}
+
+variable "internal_port" {
+    type = number
+    default = 1880
+}
+
+variable "container_count" {
+    type = number
+    default = 2
+}
+
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
 resource "random_string" "random" {
-  count = 4
+  count = var.container_count
   length = 5
   special = false
   upper = false
 }
 
  resource "docker_container" "node_container" {
-   count = 2
+   count = var.container_count
    name  = join("-", ["nodered", random_string.random[count.index].result])
    image = docker_image.nodered_image.latest
    ports {
-     internal = 1880
+     internal = var.internal_port
+#     external = var.external_port
    }
  }
 
-output "ip-address" {
-  value       =  [ for i in docker_container.node_container[*]: join(":", [i.ip_address], i.ports[*]["external"]) ]
-  description = "The IP address and external port of the container"
-}
+ output "ip-address" {
+   value       =  [ for i in docker_container.node_container[*]: join(":", [i.ip_address], i.ports[*]["external"]) ]
+   description = "The IP address and external port of the container"
+ }
+ 
+ output "container-name" {
+   value       = docker_container.node_container[*].name
+   description = "The name of the container"
+ }
 
-output "container-name" {
-  value       = docker_container.node_container[*].name
-  description = "The name of the container"
-}
